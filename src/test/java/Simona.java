7ba13ef84decc560d965434806276d5628897cb7
AnnaSokolenko <aobrian63@gmail.com>
2022-06-23 20:41:30 +0300

    NanaBanana

---
+++ src/test/java/Simona.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Simona {
+
+
+
+    @Test
+
+    public void doit() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.youtube.com/");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Qa automation");
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys(Keys.RETURN);
+    }
+
+}
